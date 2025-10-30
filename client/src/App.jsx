c73094798821e59e43bf15ac9bfe986765433204
adamchukyurii <adamchukyurii5002@gmail.com>
2025-10-30 21:57:44 +0100

    structure for project.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import AppRoutes  from './pages';
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+    <AppRoutes/>
+    </>                                                                                                                                 
+  )
+}                                                                                                                                                                                                                                    
+
+export default App
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage.jsx";
+import ShoppingList from "./ShoppingList.jsx"
+
+export default function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </>
+  );
+}
